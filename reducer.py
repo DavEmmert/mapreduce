638e6dfd5514a6c131e82294db46ab809f3935d0
DavEmmert <david_emmert@seneca2.lehre.hwr-berlin.de>
2025-07-15 18:33:02 +0200

    Excercis 6

--- reducer.py
+++ reducer.py
@@ -20,6 +20,7 @@ import sys
 
 # Sum of all sales (values) is initialized with zero, we just started
 sum_of_values = 0
+count_values = 0
 
 # Previous key is initialized with None, we just started
 previous_key = None
@@ -40,23 +41,24 @@ for line in sys.stdin:
     # This means the line starts with a new key (key changes e.g. from "Visa" to "Cash")
     # Remember that our keys are sorted
     if previous_key != None and previous_key != key:
-	if sum_of_values > 114:        # Then write the result of the old key (Key=category, Value= Sum of Sales)
+        # Then write the result of the old key (Key=category, Value= Sum of Sales)
         # to the standart output (stdout)
         # Key and value are seperated by a tab (\t)
         # Line ends with new line (\n)
-       	    sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values))
+        sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values/count_values))
         # Sum of sales starts again with 0
-            sum_of_values = 0
-
+        sum_of_values = 0
+	count_values = 0
     # Add the value to the total sales
     # a += b is the same as a = a + b
     # the float function transforms the value
     # to a float data type (like decimal)
-    sum_of_values += 1
+    sum_of_values +=  float(value)
+    count_values += 1
+	
     # float(value)
     # the previous key for the next iteration is the current key of the this iteration 
     previous_key = key
 
 # write the last result to stdout
-if sum_of_values > 114:
-    sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values))
+sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values/count_values))
